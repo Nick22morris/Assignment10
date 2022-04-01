0c6a7f3e624eebb6e4be5673f374cfdb2240fcd4
Nick <65043773+Nick22morris@users.noreply.github.com>
2022-04-01 14:54:56 -0500

    Added work method in runner

--- Runner.java
+++ Runner.java
@@ -1,11 +1,11 @@
-
 public class Runner {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Employee bossMan = new SalariedEmployee("Mr. Bossman",2001, 40000.00);
 		Employee poorManWeHate = new HourlyEmployee("Mr. noWorkersRights",2001,8.0,38.0);
+		((HourlyEmployee) poorManWeHate).work(45);
 		Employee poorManWeLove = new OvertimeHourlyEmployee("Jose",2001,8.0,70.0);
+		((OvertimeHourlyEmployee) poorManWeLove).work(45);
 		System.out.println(bossMan);
 		System.out.println(poorManWeHate);
 		System.out.println(poorManWeLove);
